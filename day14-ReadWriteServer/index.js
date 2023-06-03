32f435410e757c303b804c5e90130b70378f4c61
hanshir.hamza <Hanshir.Hamza@consultant.mannai.com.qa>
2023-06-03 16:36:46 +0300

    day14

---
+++ day14-ReadWriteServer/index.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+fs.writeFileSync("file1.txt", "This is the content");
+console.log("file1 added");
+
+
+// // Write
+// const data = fs.writeFileSync("user.json", JSON.stringify(userList, undefined, 4));
+// return;
+// // Exist
+// // const isExist = fs.existsSync("file2.txt");
+// // Read
+// const data = fs.readFileSync("user.json", "utf8");
+
+// // fs.appendFileSync("file.txt", "This is a new text");
+// // console.log(JSON.parse(data));
+
+// // fs.unlinkSync("file.txt")
+
+// // fs.mkdirSync("api");
+// fs.rmdirSync("api");
+
+// fs.writeFile("file1.txt", "This is Plain text", (err) => {
+//   if (err) throw err;
+//   console.log("Writing Completed");
+// });
+
+// fs.readFile("file1.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+//   console.log("Writing Completed");
+// });
+
+// fs.access("file1.txt", (err) => {
+//   if (err) throw err;
+
+//   console.log("File Exist");
+// });
